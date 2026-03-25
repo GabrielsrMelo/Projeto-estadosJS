fabf4e71014084eac45a332133fc7d42cbba60b0
GABRIEL SOUSA RODRIGUES DE MELO <25203630@CFP127ALUNO.LOCAL>
2026-03-25 09:07:54 -0300

    feat: Código e funções finalizadas com sucesso

--- Modulo/localidades.js
+++ Modulo/localidades.js
@@ -17,7 +17,8 @@ const getListaDeEstados = function(){
 }
 
 const getDadosEstados = function(uf){
-    let resultado 
+    let resultado
+    let status = false 
 
     listaDeEstados.estados.forEach(function(descricao){
         
@@ -28,25 +29,34 @@ const getDadosEstados = function(uf){
                 capital: descricao.capital,
                 regiao: descricao.regiao
             }
-
+            status = true
         }
 
     })
-    return resultado
+    if(status)
+        return resultado
+    else
+        return false
 }
 const getCapitalEstado = function(uf){
     let resultado
+    let status = false
 
     listaDeEstados.estados.forEach(function(capital){
-        if(capital.sigla.toUpperCase() === uf.toUpperCase())
+        if(capital.sigla.toUpperCase() === uf.toUpperCase()){
             resultado = {uf: capital.sigla, descricao: capital.nome, capital: capital.capital}
-        
+        status = true
+        }
     })
-    return resultado
+    if(status)
+        return resultado
+    else
+        return false
 }
 
 const getEstadosRegiao = function(reg){
     let resultado = []
+    let status = false
 
     listaDeEstados.estados.forEach(function(area){
         if(area.regiao.toUpperCase() === reg.toUpperCase()){
@@ -54,22 +64,49 @@ const getEstadosRegiao = function(reg){
                 uf: area.sigla,
                 descricao: area.nome
             })
+            status = true
         }
 
     })
-    return {
-        regiao: reg,
-        estados: resultado
+    if(status){
+        return {
+            regiao: reg,
+            estados: resultado
+        }
+    }else{
+        return false
     }
-
 }
 
 const getCapitalPais = function(){
+    let capitais = []
+    let retornoJSON = {}
+
+
+    listaDeEstados.estados.forEach(function(capitaisBr){
+        if(capitaisBr.capital_pais){
+            capitais.push({
+                capital_atual: capitaisBr.capital_pais.capital,
+                uf: capitaisBr.sigla,
+                descricao: capitaisBr.nome,
+                capital: capitaisBr.capital,
+                regiao: capitaisBr.regiao,
+                capital_pais_ano_inicio: capitaisBr.capital_pais.ano_inicio,
+                capital_pais_ano_termino: capitaisBr.capital_pais.ano_fim
+                
+            })
+        }
+    })
+
+    retornoJSON.capitais = capitais
+    return retornoJSON
 
 }
 
 const getCidades = function(uf){
     let cidade = []
+    let status = false
+    
 
     listaDeEstados.estados.forEach(function(sEstado){
         if(sEstado.sigla === uf){
@@ -79,11 +116,18 @@ const getCidades = function(uf){
                 quantidade_cidades: sEstado.cidades.length,
                 cidades: sEstado.cidades.map(cidade => cidade.nome )
             }
+            status = true
         }
 
     })
-
-    return cidade
+    if(status)
+        return cidade
+    else
+        return false
 }
+// console.log(getListaDeEstados())
+// console.log(getDadosEstados('SP'))
+// console.log(getCapitalEstado('AC'))
 // console.log(getEstadosRegiao('norte'))
-console.log(getCidades('AC'))
+// console.log(getCapitalPais())
+// console.log(getCidades('AC'))
